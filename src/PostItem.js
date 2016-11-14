8c3028d713470411846d9f07bdf980df12b79f76
codinronan <patrick@wizeline.com>
2016-11-14 10:34:50 -0600

    Rearranged PostItem to match the mock

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
 function PostItem(props) {
-  const { title, thumbUrl, voteCount, commentCount, createdDate, createdBy, subreddit } = props;
+  const { title, thumbUrl, voteCount, commentCount, createdDate, createdBy, subreddit, threadId } = props;
   return (
     <div className='post'>
       <div className='post-item'>
-        <div className='post-item_votes'>
-          <span className='post-item_votes--text'>{voteCount}</span>
-        </div>
-        <div className='post-item_thumb'>
-          <img src={thumbUrl} className='post-item_thumb--img' alt='Thread thumbnail' />
+        <div className='post-item_header'>
+          <div className='post-item_thumb'>
+            <img src={thumbUrl} className='post-item_thumb--img' alt='Thread thumbnail' />
+          </div>
+          <div className='post-item_createdinfo'>
+            <span className='post-item_createdinfo--by'>{createdBy}</span>
+            <span className='post-item_createdinfo--date'>{createdDate}</span>
+            <span className='post-item_createdinfo--subreddit'>To {subreddit}</span>
+          </div>
         </div>
         <div className='post-item_body'>
-          <div className='post-item_body--title'>
-            <span>{title}</span>
-          </div>
-          <div className='post-item_body--data'>
-            <span className='post-item_body--data--age'>{createdDate}</span>
-            <span className='post-item_body--data--createdby'>{createdBy}</span>
-            <span className='post-item_body--data--subreddit'>{subreddit}</span>
+          <span className='post-item_body--title'>{title}</span>
+          <div className='post-item_body--jumplinks'>
+            <span className='post-item_body--comments'>{commentCount} comments</span>
+            <span className='post-item_body--votes'>{voteCount} votes</span>
           </div>
           <div className='post-item_body--jumplinks'>
-            <span className='post-item_body--jumplinks-commentcount'>{commentCount}</span>
+            <a className='post-item_body--jumplinks-commentlink' href={`/comment/${threadId}`}>Comment</a>
+            <a className='post-item_body--jumplinks-sharelink' href='#'>Share</a>
           </div>
         </div>
       </div>
