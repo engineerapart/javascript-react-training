183baca3f5d1e8d67054d4f349924f9264a57823
codinronan <patrick@wizeline.com>
2016-11-16 16:32:11 -0600

    Inline styles

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,21 @@ import { Link } from 'react-router';
 
 import logo from './logo.svg';
 
+const styles = {
+  header: {
+    backgroundColor: '#222',
+    height: 150,
+    padding: 20,
+    color: 'white',
+  }
+};
+
 function Header(props) {
   return (
-    <div className="app-header">
+    <div style={styles.header}>
       <img src={logo} className="app-logo" alt="logo" />
       <h2 style={{fontWeight: 'bold'}}>Welcome to React</h2>
-      <Link to='posts' activeStyle={{color: 'red'}}>Go to Posts</Link>
+      <Link to='posts' activeStyle={{color: 'red'}} activeClassName='whatever'>Go to Posts</Link>
     </div>
   );
 }
