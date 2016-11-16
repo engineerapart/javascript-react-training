0b10d631775b579b0d4cdbf5045a688e9c4ca05c
codinronan <patrick@wizeline.com>
2016-11-16 13:08:32 -0600

    Added the Link to Header to demonstrate auto-active state

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
+
 import logo from './logo.svg';
 
 function Header(props) {
   return (
     <div className="app-header">
       <img src={logo} className="app-logo" alt="logo" />
-      <h2>Welcome to React</h2>
+      <h2 style={{fontWeight: 'bold'}}>Welcome to React</h2>
+      <Link to='posts' activeStyle={{color: 'red'}}>Go to Posts</Link>
     </div>
   );
 }
